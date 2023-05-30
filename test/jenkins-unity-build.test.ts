19665a4551c2a17824d0bf98021f290a6db55e9a
tmokmss <tmokmss@users.noreply.github.com>
2023-05-30 15:53:57 +0900

    fix test

--- test/jenkins-unity-build.test.ts
+++ test/jenkins-unity-build.test.ts
@@ -1,8 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { JenkinsUnityBuildStack } from '../lib/jenkins-unity-build-stack';
+import { readdirSync, rmSync } from 'fs';
+import { join } from 'path';
 
 test('Snapshot test', () => {
+  // remove all the intermediate files that are created before the test
+  const jenkinsResourceDir = join(__dirname, '..', 'lib', 'construct', 'jenkins', 'resources', 'config');
+  readdirSync(jenkinsResourceDir)
+    .filter((path) => path.match(/^jenkins.+yaml$/) != null)
+    .forEach((path) => rmSync(join(jenkinsResourceDir, path)));
+
   const app = new cdk.App();
   const stack = new JenkinsUnityBuildStack(app, 'TestStack', {
     env: {
