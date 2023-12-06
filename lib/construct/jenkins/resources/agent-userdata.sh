716b61609bbb6c3d2a74736108f277d392e266f0
tmokmss <tomookam@live.jp>
2023-12-06 21:19:15 +0900

    randomize ebs volume to use

--- lib/construct/jenkins/resources/agent-userdata.sh
+++ lib/construct/jenkins/resources/agent-userdata.sh
@@ -11,14 +11,20 @@ TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metad
 AZ=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .availabilityZone`
 INSTANCE_ID=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id`
 REGION=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/region`
-VOLUME_ID=$(aws ec2 describe-volumes --filters Name=tag:Kind,Values=<KIND_TAG> Name=availability-zone,Values=$AZ Name=status,Values=available --query 'Volumes[0].VolumeId' --output text --region $REGION)
+# find a volume with the same kind tag as the instance
+# choose a random volume to use evenly across all volumes of the same kind
+VOLUME_ID=$(aws ec2 describe-volumes --filters Name=tag:Kind,Values=<KIND_TAG> Name=availability-zone,Values=$AZ Name=status,Values=available --query 'Volumes[*].[VolumeId]' --output text --region $REGION | shuf | head -1)
 
 if [ "$VOLUME_ID" ];then
-    echo "found volume ${VOLUME}"
+    echo "found volume $VOLUME_ID"
     DEVICE_NAME="/dev/xvdf"
-    # There is possibly a race condition between other instances.
-    # We may want to retry attach-volume according to the return code (currently omitted).
     aws ec2 attach-volume --device $DEVICE_NAME --instance-id $INSTANCE_ID --volume-id $VOLUME_ID --region $REGION
+    if [ $? -ne 0 ]; then
+        # There is possibly a race condition between other instances (e.g. the volume is occupied by another instance).
+        # Terminate the instance if attaching volume failed. ASG will retry booting an instance.
+        shutdown now -h
+    fi
+
     # we should do polling for the volume status instead, but it usually finishes in a few seconds...
     sleep 10
 
